c7cefdb721a9c93d9c3fb30acc6864f37ee6dae6
ThiagoMonts <thiagohonorato@hotmail.com>
2023-11-17 11:22:02 -0300

    Base de dados H2 e criação do Cliente

---
+++ ecommerce/src/main/java/com/uniesp/ecommerce/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.uniesp.ecommerce.repositories;
+
+import com.uniesp.ecommerce.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
